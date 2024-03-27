0b8db14edfcb9d84d791aa08b01aa00a4fb10ee7
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-03-27 14:10:52 -0300

    Show teams read from database

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,31 @@
+import { useLoaderData, Links, json } from "@remix-run/react"
+import { prisma } from "~/only.server/db"
+
+export const loader = async () => {
+  const data = {
+    teams: await prisma.team.findMany(),
+  }
+
+  return json(data)
+}
+
 export default function Index() {
+  const { teams } = useLoaderData<typeof loader>()
+
   return (
-    <div>
-      <h1>I'll soon be connected to SQLite</h1>
-    </div>
+    <>
+      <div>
+        <h1>Soccer Teams around the world</h1>
+      </div>
+      <ul>
+        {teams.map(team => (
+          <li key={team.id}>
+            <span>{team.team}</span>
+            |
+            <span>{team.country}</span>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
