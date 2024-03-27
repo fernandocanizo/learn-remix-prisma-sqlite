2c2fe168d34ab2e3d0d98fec86d42d558e8b341a
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-03-27 13:58:47 -0300

    Add db module

---
+++ app/only.server/db.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+export let prisma
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+} else {
+  if (!globalThis.prisma) {
+    globalThis.prisma = new PrismaClient()
+  }
+  prisma = globalThis.prisma
+}
